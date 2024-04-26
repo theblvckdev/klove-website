4f3f56364f6bcff83bd95bd7c46919fa33f87c54
theblvckdev <nathancodes05@gmail.com>
2024-04-26 11:23:40 +0100

    updated button code [added a white button variant, and changed primary button hover background to white]. Also refactored button unit test code > still not working though.

--- src/components/global_ui_components/button/button.jsx
+++ src/components/global_ui_components/button/button.jsx
@@ -5,21 +5,33 @@ const Button = ({ body, variant }) => {
     <>
       {variant === "primary" ? (
         <button
-          data-test-id="button"
-          className="py-3 px-7 bg-primary-0 rounded-md flex items-center justify-center font-rubik text-white hover:bg-black-0 duration-300 ease-in shadow"
+          data-testid="button"
+          className="py-3 px-5 text-sm bg-primary-0 rounded-md flex items-center justify-center font-rubik text-white hover:bg-white hover:text-gray-900 duration-300 ease-in shadow-lg capitalize tracking-wide"
+        >
+          {body}
+        </button>
+      ) : variant === "white" ? (
+        <button
+          data-testid="button"
+          className="py-3 px-5 text-sm rounded-md bg-white flex items-center justify-center font-rubik text-gray-900 hover:bg-primary-0 hover:text-white duration-300 ease-in capitalize tracking-wide"
         >
           {body}
         </button>
       ) : (
         <button
-          data-test-id="button"
-          className="py-3 px-5 rounded-md bg-transparent flex items-center justify-center font-rubik text-white "
+          data-testid="button"
+          className="py-3 px-5 text-sm rounded-md bg-transparent flex items-center justify-center font-rubik text-white capitalize tracking-wide"
         >
           {body}
         </button>
       )}
     </>
   );
+};
+
+Button.default = {
+  body: "button text",
+  variant: "primary",
 };
 
 export default Button;
--- src/components/global_ui_components/button/button.test.js
+++ src/components/global_ui_components/button/button.test.js
@@ -1,8 +1,21 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import Button from "./button";
 
-test("button variant prop must be either primary or transparent", () => {
-  render(<Button />);
-  const buttonVariantPropValue = screen.getAllByTestId("button");
-  expect(buttonVariantPropValue).toEqual("primary" || "transparent");
+test('renders with primary variant when variant is "primary"', () => {
+  render(<Button variant="primary" />);
+  const button = screen.getByTestId("button");
+  expect(button).toHaveClass("bg-primary-0");
+});
+
+test('renders with transparent variant when variant is "transparent"', () => {
+  render(<Button variant="transparent" />);
+  const button = screen.getByTestId("button");
+  expect(button).toHaveClass("bg-transparent");
+});
+
+test('renders with transparent variant by default when variant is neither "primary" nor "transparent"', () => {
+  render(<Button variant="invalidVariant" />);
+  const button = screen.getByTestId("button");
+  expect(button).toHaveClass("bg-transparent");
 });
