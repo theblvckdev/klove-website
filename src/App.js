e6c5a591f63f4d1d24b55279f0fd74432f58e13c
theblvckdev <nathancodes05@gmail.com>
2024-04-26 11:21:59 +0100

    setup and config of aos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      duration: 700,
+      easing: "ease-out-cubic",
+    });
+  }, []);
+
   return (
     <>
       <Router>
