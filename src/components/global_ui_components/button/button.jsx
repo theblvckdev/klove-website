0848e610ceaec9056a3e4094543b2b0d1c0d5892
theblvckdev <nathancodes05@gmail.com>
2024-04-26 04:40:43 +0100

    button component

--- src/components/global_ui_components/button/button.jsx
+++ src/components/global_ui_components/button/button.jsx
@@ -6,14 +6,14 @@ const Button = ({ body, variant }) => {
       {variant === "primary" ? (
         <button
           data-test-id="button"
-          className="py-3 px-5 bg-primary-0 rounded-md flex items-center justify-center font-rubik text-white hover:bg-black-0"
+          className="py-3 px-7 bg-primary-0 rounded-md flex items-center justify-center font-rubik text-white hover:bg-black-0 duration-300 ease-in shadow"
         >
           {body}
         </button>
       ) : (
         <button
           data-test-id="button"
-          className="py-3 px-5 rounded-md bg-transparent flex items-center justify-center font-rubik text-white"
+          className="py-3 px-5 rounded-md bg-transparent flex items-center justify-center font-rubik text-white "
         >
           {body}
         </button>
--- src/components/global_ui_components/button/button.test.js
+++ src/components/global_ui_components/button/button.test.js
@@ -3,6 +3,6 @@ import Button from "./button";
 
 test("button variant prop must be either primary or transparent", () => {
   render(<Button />);
-  const buttonVariantProp = screen.getAllByTestId("button");
-  expect(buttonVariantProp).toEqual("primary" || "transparent");
+  const buttonVariantPropValue = screen.getAllByTestId("button");
+  expect(buttonVariantPropValue).toEqual("primary" || "transparent");
 });
