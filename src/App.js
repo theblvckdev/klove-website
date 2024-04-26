997aa904684356d9d556f33eb1124b34e0ec0f33
theblvckdev <nathancodes05@gmail.com>
2024-04-26 04:20:50 +0100

    page routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route data-test-id="route" exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
